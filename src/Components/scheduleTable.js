62280a34a2eabaf6c93e2b0f31f7f325fba7d870
warpfox <tylermwatts@gmail.com>
2019-06-14 13:18:52 -0500

    wip

---
+++ src/Components/scheduleTable.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const scheduleTable = () => {
+    <tr>
+        <th>{"Employee"}</th>
+        <th>{"Monday"}</th>
+        <th>{"Tuesday"}</th>
+        <th>{"Wednesday"}</th>
+        <th>{"Thursday"}</th>
+        <th>{"Friday"}</th>
+        <th>{"Saturday"}</th>
+        <th>{"Sunday"}</th>
+    </tr>
+}
