c8d8bd2a116cd68c47803b908219038f9e558f4f
warpfox <tylermwatts@gmail.com>
2019-06-17 14:46:43 -0500

    route config

--- src/server/server.js
+++ src/server/server.js
@@ -3,21 +3,15 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const employeeService = require('./services/employeeService');
+const routeConfig = require('./routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/getOne', (req, res) => {
-  req.body.id = '5d07bfbba977fc04e4676343';
-  return employeeService.getEmployeeById(req, res);
-});
-
-app.get('/getAll', (req, res) => {
-  return employeeService.getAllEmployees(req, res);
-});
+routeConfig(app);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(process.env.PORT || 3001);
