68ff76eb37cde2645e0a4e29fe1ab0a7fd3a23aa
warpfox <tylermwatts@gmail.com>
2019-06-17 18:47:32 -0500

    wip

--- src/Components/AddEmployeeForm/AddEmployeeForm.js
+++ src/Components/AddEmployeeForm/AddEmployeeForm.js
@@ -4,38 +4,38 @@ import AddEmployeeShifts from '../AddEmployeeShifts/AddEmployeeShifts';
 import * as styles from './AddEmployeeForm.module.css';
 
 const AddEmployeeForm = ({ addEmployee }) => {
-  const [name, setName] = useState(singleEmployee.name);
-  const [schedule, setSchedule] = useState(singleEmployee.schedule);
+	const [name, setName] = useState(singleEmployee.name);
+	const [schedule, setSchedule] = useState(singleEmployee.schedule);
 
-  const handleNameChange = e => {
-    setName(e.target.value);
-  };
+	const handleNameChange = e => {
+		setName(e.target.value);
+	};
 
-  const handleScheduleChange = newSchedule => {
-    addEmployee({ name: name, schedule: newSchedule });
-    initializeState();
-    document.getElementById('add-employee-form').reset();
-  };
+	const handleScheduleChange = newSchedule => {
+		addEmployee({ name: name, schedule: newSchedule });
+		initializeState();
+		document.getElementById('add-employee-form').reset();
+	};
 
-  const initializeState = () => {
-    setName(singleEmployee.name);
-    setSchedule(singleEmployee.schedule);
-  };
+	const initializeState = () => {
+		setName(singleEmployee.name);
+		setSchedule(singleEmployee.schedule);
+	};
 
-  return (
-    <div className={styles.formBorder}>
-      <h3>Add a new employee</h3>
-      <form id="add-employee-form">
-        <label>{'Employee Name: '}</label>
-        <input type="text" value={name} onChange={handleNameChange} required />
-        <br />
-        <AddEmployeeShifts
-          schedule={schedule}
-          scheduleChange={handleScheduleChange}
-        />
-      </form>
-    </div>
-  );
+	return (
+		<div className={styles.formBorder}>
+			<h3>Add a new employee</h3>
+			<form id="add-employee-form">
+				<label>{'Employee Name: '}</label>
+				<input type="text" value={name} onChange={handleNameChange} required />
+				<br />
+				<AddEmployeeShifts
+					schedule={schedule}
+					scheduleChange={handleScheduleChange}
+				/>
+			</form>
+		</div>
+	);
 };
 
 export default AddEmployeeForm;
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,12 +1,13 @@
 const employeeService = require('./services/employeeService');
 
 module.exports = app => {
-  app.route('/api/employees').get(employeeService.getAllEmployees);
+	app.route('/api/employees').get(employeeService.getAllEmployees);
 
-  app
-    .route('/api/employee/:id')
-    .get(employeeService.getEmployeeById)
-    .post(employeeService.addEmployee)
-    .put(employeeService.editEmployee)
-    .delete(employeeService.deleteEmployee);
+	app.route('/api/employee').post(employeeService.addEmployee);
+
+	app
+		.route('/api/employee/:id')
+		.get(employeeService.getEmployeeById)
+		.put(employeeService.editEmployee)
+		.delete(employeeService.deleteEmployee);
 };
