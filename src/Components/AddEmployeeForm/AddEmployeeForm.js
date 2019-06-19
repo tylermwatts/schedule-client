181a4b1972017b364ddf7872a34e95bee550f0b0
warpfox <tylermwatts@gmail.com>
2019-06-19 15:31:35 -0500

    wip

--- src/Components/AddEmployeeForm/AddEmployeeForm.js
+++ src/Components/AddEmployeeForm/AddEmployeeForm.js
@@ -4,60 +4,61 @@ import ShiftSelection from '../ShiftSelection/ShiftSelection';
 import * as styles from './AddEmployeeForm.module.css';
 
 const AddEmployeeForm = ({ addEmployee, newEmployee }) => {
-  const [name, setName] = useState(newEmployee.name);
-  const [schedule, setSchedule] = useState({ ...newEmployee.schedule });
+	const [name, setName] = useState(newEmployee.name);
+	const [schedule, setSchedule] = useState({ ...newEmployee.schedule });
 
-  const handleNameChange = e => {
-    setName(e.target.value);
-  };
+	const handleNameChange = e => {
+		setName(e.target.value);
+	};
 
-  const handleScheduleChange = (e, day, key) => {
-    setSchedule({
-      ...schedule,
-      [day]: {
-        ...schedule[day],
-        [key]: { value: e.target.value }
-      }
-    });
-  };
+	const handleScheduleChange = (e, day, key) => {
+		setSchedule({
+			...schedule,
+			[day]: {
+				...schedule[day],
+				[key]: { value: e.target.value },
+			},
+		});
+	};
 
-  const handleSubmit = () => {
-    addEmployee({ name, schedule });
-    initializeState();
-    document.getElementById('add-employee-form').reset();
-  };
+	const handleSubmit = e => {
+		e.preventDefault();
+		addEmployee({ name, schedule });
+		initializeState();
+		document.getElementById('add-employee-form').reset();
+	};
 
-  const initializeState = () => {
-    setName('');
-    setSchedule({ ...newEmployee.schedule });
-  };
+	const initializeState = () => {
+		setName('');
+		setSchedule({ ...newEmployee.schedule });
+	};
 
-  return (
-    <div className={styles.container}>
-      <h3>Add a new employee</h3>
-      <form
-        className={styles.formContainer}
-        id="add-employee-form"
-        onSubmit={handleSubmit}
-      >
-        <label>{'Employee Name: '}</label>
-        <input type="text" value={name} onChange={handleNameChange} required />
-        <br />
-        <>
-          {Object.keys(days).map(d => {
-            return (
-              <ShiftSelection
-                key={d}
-                day={d}
-                handleScheduleChange={handleScheduleChange}
-              />
-            );
-          })}
-        </>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<h3>Add a new employee</h3>
+			<form
+				className={styles.formContainer}
+				id="add-employee-form"
+				onSubmit={handleSubmit}
+			>
+				<label>{'Employee Name: '}</label>
+				<input type="text" value={name} onChange={handleNameChange} required />
+				<br />
+				<>
+					{Object.keys(days).map(d => {
+						return (
+							<ShiftSelection
+								key={d}
+								day={d}
+								handleScheduleChange={handleScheduleChange}
+							/>
+						);
+					})}
+				</>
+				<input type="submit" value="Submit" />
+			</form>
+		</div>
+	);
 };
 
 export default AddEmployeeForm;
--- src/Components/ScheduleContainer/ScheduleContainer.js
+++ src/Components/ScheduleContainer/ScheduleContainer.js
@@ -5,30 +5,36 @@ import AddEmployeeForm from '../AddEmployeeForm/AddEmployeeForm';
 import ScheduleTable from '../ScheduleTable/ScheduleTable';
 
 const ScheduleContainer = () => {
-  const [employees, setEmployees] = useState([]);
-  const newEmployee = singleEmployee;
+	const [employees, setEmployees] = useState([]);
+	const newEmployee = singleEmployee;
 
-  useEffect(() => {
-    const getData = () => {
-      employeeRepository.getAllEmployees().then(data => setEmployees(data));
-    };
-    getData();
-  }, [employees]);
+	useEffect(() => {
+		const getData = () => {
+			employeeRepository.getAllEmployees().then(data => setEmployees(data));
+		};
+		getData();
+	}, [employees]);
 
-  const addEmployee = newEmployee => {
-    employeeRepository.addEmployee(newEmployee);
-  };
+	const addEmployee = newEmployee => {
+		employeeRepository
+			.addEmployee(newEmployee)
+			.then(response => console.log(response))
+			.catch(err => console.log(error));
+	};
 
-  const deleteEmployee = id => {
-    employeeRepository.deleteEmployee(id);
-  };
+	const deleteEmployee = id => {
+		employeeRepository
+			.deleteEmployee(id)
+			.then(response => alert(response.success))
+			.catch(err => console.log(err));
+	};
 
-  return (
-    <>
-      <ScheduleTable employees={employees} deleteEmployee={deleteEmployee} />
-      <AddEmployeeForm addEmployee={addEmployee} newEmployee={newEmployee} />
-    </>
-  );
+	return (
+		<>
+			<ScheduleTable employees={employees} deleteEmployee={deleteEmployee} />
+			<AddEmployeeForm addEmployee={addEmployee} newEmployee={newEmployee} />
+		</>
+	);
 };
 
 export default ScheduleContainer;
