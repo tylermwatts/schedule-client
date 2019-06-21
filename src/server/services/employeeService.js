c5832ee256c1b5c0a9d51bb18bdf29b39893db9e
warpfox <tylermwatts@gmail.com>
2019-06-21 13:15:53 -0500

    catch error on delete

--- src/server/services/employeeService.js
+++ src/server/services/employeeService.js
@@ -6,74 +6,71 @@ const url = process.env.DB_URI;
 const assert = require('assert');
 
 module.exports = {
-  getAllEmployees(req, res) {
-    mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
-      assert.equal(null, err);
-      const db = client.db('employees');
-      db.collection('employee')
-        .find({})
-        .toArray()
-        .then(response => res.json(response));
-    });
-  },
+	getAllEmployees(req, res) {
+		mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
+			assert.equal(null, err);
+			const db = client.db('employees');
+			db.collection('employee')
+				.find({})
+				.toArray()
+				.then(response => res.json(response));
+		});
+	},
 
-  getEmployeeById(req, res) {
-    mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
-      assert.equal(null, err);
-      const db = client.db('employees');
-      db.collection('employee')
-        .findOne({ _id: new ObjectID(req.params.id) })
-        .then(response => res.json(response));
-    });
-  },
+	getEmployeeById(req, res) {
+		mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
+			assert.equal(null, err);
+			const db = client.db('employees');
+			db.collection('employee')
+				.findOne({ _id: new ObjectID(req.params.id) })
+				.then(response => res.json(response));
+		});
+	},
 
-  addEmployee(req, res) {
-    const employee = {
-      _id: new ObjectID(),
-      name: req.body.name,
-      schedule: req.body.schedule
-    };
-    mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
-      assert.equal(null, err);
-      const db = client.db('employees');
-      db.collection('employee')
-        .insertOne(employee)
-        .then(res.json({ employee }));
-      client.close();
-    });
-  },
+	addEmployee(req, res) {
+		const employee = {
+			_id: new ObjectID(),
+			name: req.body.name,
+			schedule: req.body.schedule,
+		};
+		mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
+			assert.equal(null, err);
+			const db = client.db('employees');
+			db.collection('employee')
+				.insertOne(employee)
+				.then(res.json({ employee }));
+			client.close();
+		});
+	},
 
-  editEmployee(req, res) {
-    const name = req.body.name;
-    const schedule = req.body.schedule;
-    mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
-      assert.equal(null, err);
-      const db = client.db('employees');
-      db.collection('employee')
-        .findOneAndUpdate(
-          { _id: new ObjectID(req.params.id) },
-          { $set: { name, schedule } }
-        )
-        .then(response => res.json(response));
-      client.close();
-    });
-  },
+	editEmployee(req, res) {
+		const name = req.body.name;
+		const schedule = req.body.schedule;
+		mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
+			assert.equal(null, err);
+			const db = client.db('employees');
+			db.collection('employee')
+				.findOneAndUpdate(
+					{ _id: new ObjectID(req.params.id) },
+					{ $set: { name, schedule } }
+				)
+				.then(response => res.json(response));
+			client.close();
+		});
+	},
 
-  deleteEmployee(req, res) {
-    const employeeId = new ObjectID(req.params.id);
-    mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
-      assert.equal(null, err);
-      const db = client.db('employees');
-      db.collection('employee')
-        .findOneAndDelete({ _id: employeeId })
-        .then(foundEmployee => {
-          if (!foundEmployee.value) {
-            res.json({ error: 'employee not found' });
-          } else {
-            res.json({ success: `${foundEmployee.value.name} deleted.` });
-          }
-          client.close();
-        });
-    });
-  }
+	deleteEmployee(req, res) {
+		const employeeId = new ObjectID(req.params.id);
+		mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
+			assert.equal(null, err);
+			const db = client.db('employees');
+			db.collection('employee')
+				.findOneAndDelete({ _id: employeeId })
+				.then(foundEmployee => {
+					res.json({ success: `${foundEmployee.value.name} deleted.` });
+				})
+				.catch(err => console.log(err));
+			client.close();
+		});
+	},
 };
