1432d3cb6411c5851dc7af5bd00785962a7ada97
Warpfox <tylermwatts@gmail.com>
2019-06-21 20:43:50 -0500

    inline dropdowns for editing schedule

--- src/Components/EditableDropdowns/EditableDropdowns.js
+++ src/Components/EditableDropdowns/EditableDropdowns.js
@@ -1,65 +1,119 @@
 // This is an experimental component that I am using to work out how I want to handle
 // being able to edit an employee's schedule with inline dropdown selects
 
-import React from 'react';
+import React, { useState } from 'react';
+import { assignments, days, shifts } from '../../data/';
+import { singleEmployee } from '../../initial_state';
 import * as styles from './EditableDropdowns.module.css';
 
-const EditableDropdowns = () => {
-  // TODO: Update changes in state onChange
+const EditableDropdowns = ({ employee = singleEmployee }) => {
+  const [name, setName] = useState('');
+  const [schedule, setSchedule] = useState(employee.schedule);
+
+  const handleScheduleChange = (e, day, key) => {
+    setSchedule({
+      ...schedule,
+      [day]: {
+        ...schedule[day],
+        [key]: { value: e.target.value }
+      }
+    });
+  };
+
+  const handleSave = () => {
+    // TODO: send the updated employee to the database
+  };
+
   return (
     <tr>
-      <td>Name</td>
       <td>
-        <div>
-          <span>
-            <select className={styles.select}>
-              <option>OFF</option>
-              <option>8:30 - 5</option>
-              <option>9 - 5:30</option>
-              <option>10 - 6:30</option>
-              <option>11 - 7:30</option>
-              <option>12 - 8:30</option>
-            </select>
-          </span>
-          <span>
-            <select className={styles.select}>
-              <option>--</option>
-              <option>Manager</option>
-              <option>Book Desk</option>
-              <option>Project</option>
-              <option>Large Order</option>
-              <option>Register</option>
-              <option>Greeter</option>
-              <option>Register/Greeter</option>
-              <option>Sorter</option>
-            </select>
-          </span>
-        </div>
-        <div>
-          <span>
-            <select className={styles.select}>
-              <option>--</option>
-              <option>8:30 - 5</option>
-              <option>9 - 5:30</option>
-              <option>10 - 6:30</option>
-              <option>11 - 7:30</option>
-              <option>12 - 8:30</option>
-            </select>
-          </span>
-          <span>
-            <select className={styles.select}>
-              <option>--</option>
-              <option>Manager</option>
-              <option>Book Desk</option>
-              <option>Project</option>
-              <option>Large Order</option>
-              <option>Register</option>
-              <option>Greeter</option>
-              <option>Register/Greeter</option>
-              <option>Sorter</option>
-            </select>
-          </span>
-        </div>
+        <input
+          type="text"
+          placeholder="Enter name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          size="10"
+        />
+      </td>
+      {Object.keys(days).map(d => {
+        return (
+          <td key={d}>
+            <div>
+              <span>
+                <select
+                  name={`${d} shift1`}
+                  onChange={e => handleScheduleChange(e, d, 'shift1')}
+                  className={styles.select}
+                  defaultValue={schedule[d].shift1.value || ''}
+                >
+                  {shifts.map(s1 => {
+                    return (
+                      <option key={`1 ${d} ${s1}`} value={s1}>
+                        {s1}
+                      </option>
+                    );
+                  })}
+                </select>
+              </span>
+              <span>
+                <select
+                  name={`${d} assignment1`}
+                  onChange={e => handleScheduleChange(e, d, 'assignment1')}
+                  className={styles.select}
+                  defaultValue={schedule[d].assignment1.value || ''}
+                >
+                  <option>--</option>
+                  {assignments.map(a1 => {
+                    return (
+                      <option key={`1 ${d} ${a1}`} value={a1}>
+                        {a1}
+                      </option>
+                    );
+                  })}
+                </select>
+              </span>
+            </div>
+            <div>
+              <span>
+                <select
+                  name={`${d} shift2`}
+                  onChange={e => handleScheduleChange(e, d, 'shift2')}
+                  className={styles.select}
+                  defaultValue={schedule[d].shift2.value || ''}
+                >
+                  <option value="">{'--'}</option>
+                  {shifts.map(s2 => {
+                    return (
+                      <option key={`2 ${d} ${s2}`} value={s2}>
+                        {s2}
+                      </option>
+                    );
+                  })}
+                </select>
+              </span>
+              <span>
+                <select
+                  name={`${d} assignment2`}
+                  onChange={e => handleScheduleChange(e, d, 'assignment2')}
+                  className={styles.select}
+                  defaultValue={schedule[d].assignment2.value || ''}
+                >
+                  <option>--</option>
+                  {assignments.map(a2 => {
+                    return (
+                      <option key={`2 ${d} ${a2}`} value={a2}>
+                        {a2}
+                      </option>
+                    );
+                  })}
+                </select>
+              </span>
+            </div>
+          </td>
+        );
+      })}
+      <td>
+        <button onClick={handleSave}>Save</button>
       </td>
     </tr>
   );
