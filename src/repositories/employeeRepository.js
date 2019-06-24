8ec6899aac4eb7193d8c803451248cfe24ed7e93
Warpfox <tylermwatts@gmail.com>
2019-06-24 13:04:25 -0500

    updates api calls

--- src/repositories/employeeRepository.js
+++ src/repositories/employeeRepository.js
@@ -1,12 +1,15 @@
 module.exports = {
   getAllEmployees() {
-    return fetch('/api/employees', {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-    }).then(response => response.json());
+    return fetch(
+      'https://warpfox-schedule-server.herokuapp.com/api/employees',
+      {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      }
+    ).then(response => response.json());
   },
   addEmployee(employee) {
-    return fetch('/api/employee', {
+    return fetch('https://warpfox-schedule-server.herokuapp.com/api/employee', {
       method: 'POST',
       body: JSON.stringify({
         name: employee.name,
@@ -18,19 +21,26 @@ module.exports = {
       .catch(err => console.log(err));
   },
   editEmployee(employee) {
-    return fetch('/api/employee/' + employee._id, {
-      method: 'PUT',
-      body: JSON.stringify({
-        name: employee.name,
-        schedule: employee.schedule
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    }).then(response => response.json());
+    return fetch(
+      'https://warpfox-schedule-server.herokuapp.com/api/employee/' +
+        employee._id,
+      {
+        method: 'PUT',
+        body: JSON.stringify({
+          name: employee.name,
+          schedule: employee.schedule
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      }
+    ).then(response => response.json());
   },
   deleteEmployee(id) {
-    return fetch('/api/employee/' + id, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' }
-    }).then(response => response.json());
+    return fetch(
+      'https://warpfox-schedule-server.herokuapp.com/api/employee/' + id,
+      {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' }
+      }
+    ).then(response => response.json());
   }
 };
